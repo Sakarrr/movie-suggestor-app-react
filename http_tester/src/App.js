6611eef7d98e921f62ded8464d0642eff73081e9
Sakarr <ctha.sakar@gmail.com>
2024-06-26 14:59:12 +0545

    Display API data

--- http_tester/src/App.js
+++ http_tester/src/App.js
@@ -1,15 +1,42 @@
 import axios from "axios";
+import { useState } from "react";
 
 function App() {
-  const fetchMovies = () => {
+  const [movies, setMovies] = useState([]);
+
+  const fetchMovies = async () => {
     // Fetch resource
 
     console.log("Calling API");
-    const response = axios.get(
+    // const response = axios.get(
+    //   "https://api.dynoacademy.com/test-api/v1/movies"
+    // );
+
+    // console.log(response);
+
+    //   const promise = new Promise((resolve, reject) => {
+    //     const response = axios.get(
+    //       "https://api.dynoacademy.com/test-api/v1/movies"
+    //     );
+
+    //     resolve(response);
+    //   });
+
+    //   promise
+    //     .then((result) => {
+    //       console.log(result);
+    //     })
+    //     .catch((error) => {});
+
+    const response = await axios.get(
       "https://api.dynoacademy.com/test-api/v1/movies"
     );
 
     console.log(response);
+    setMovies(response.data.moviesData);
+    console.log(movies);
+
+    console.log("Finish");
   };
 
   return (
@@ -17,9 +44,12 @@ function App() {
       <button onClick={fetchMovies}>Get all movies</button>
       <br />
 
-      <div
-        style={{ background: "#e7e7e7", padding: "10px", margin: "5px" }}
-      ></div>
+      <div style={{ background: "#e7e7e7", padding: "10px", margin: "5px" }}>
+        {/* eslint-disable-next-line */}
+        {movies.map((el) => (
+          <div key={el.id}>{el.name}</div>
+        ))}
+      </div>
     </>
   );
 }
